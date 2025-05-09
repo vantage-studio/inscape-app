1cda73fe1f045158550ebaf5592389621cd5c9a1
vantage-studio <jay@vantagestudio.in>
2025-05-09 17:54:41 +0530

    module

--- vite.config.js
+++ vite.config.js
@@ -12,12 +12,30 @@ export default defineConfig({
     outDir: "dist",
     assetsDir: "assets",
     emptyOutDir: true,
+    sourcemap: true,
     rollupOptions: {
       input: {
         main: "./index.html",
+      },
+      output: {
+        manualChunks: undefined,
       },
     },
   },
   publicDir: "public",
   appType: "mpa", // Use Multi-Page Application mode
+  optimizeDeps: {
+    include: ["msw/browser"],
+  },
+  plugins: [
+    {
+      name: "configure-response-headers",
+      configureServer: (server) => {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader("Service-Worker-Allowed", "/");
+          next();
+        });
+      },
+    },
+  ],
 });
